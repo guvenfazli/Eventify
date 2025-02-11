6683d4ed6d8d66c9795292b1a455bb033e8cc494
Güven Fazlı <guven.fazli@gmail.com>
2025-02-11 08:25:20 +0300

    feat: Custom error message generator added

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ app.use(cookieparser())
 
 
 
-
+app.use((error, req, res, next) => {
+  const message = error.message
+  const statusCode = error.statusCode || 500
+  res.status(statusCode).json({ message })
+})
 
 app.listen(8080, () => {
   console.log('Server is on.')
