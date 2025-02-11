751b4e75533fff17fe4c6f2bf229b01af075d680
Güven Fazlı <guven.fazli@gmail.com>
2025-02-11 08:13:32 +0300

    chore: body parser installed, .env installed, cors installed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+
+
+
+
+
 
 
 
