20aebcf5e7e47f2f45a2d3b122c16d6df3e52776
Güven Fazlı <guven.fazli@gmail.com>
2025-02-12 06:32:12 +0300

    feat: Name editor completed. Users can send their full name in one input with their lastnames.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,20 +7,27 @@ const User = require('../models/User')
 exports.createAccount = async (req, res, next) => {
   const { name, email, password } = req.body
   const splitted = name.split(' ')
-  const lastName = splitted.length - 1
+  const surname = splitted[splitted.length - 1]
+  const fullName = name.replace(surname, '')
 
   try {
-    const foundUser = await User.findOne({ email: email })
+    const foundUser = await User.findOne({ where: { email: email } })
 
     if (foundUser) {
       throwError(410, 'User already exists!')
     }
 
-    if (!name) {
-      throwError(410, 'Testing Custom Error!')
-    }
+    const hashedPw = await bcrypt.hash(password, 12)
 
+    User.create({
+      name: fullName.trim(),
+      surname,
+      email,
+      password: hashedPw,
+    })
 
+    res.status(200).json({ message: 'Account created successfully!' })
+    return;
   } catch (err) {
     next(err)
   }
