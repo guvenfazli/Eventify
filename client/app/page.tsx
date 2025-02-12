0e809bc9939a853f651340c272cc2c0adea54b53
Güven Fazlı <guven.fazli@gmail.com>
2025-02-12 03:38:28 +0300

    feat: Auth page created, starting to register

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,9 @@
+import AppSummary from "@/components/authenticationPage/appSummary";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Project created</p>
+    <div className="flex w-full items-start justify-start">
+      <AppSummary />
     </div>
   );
 }
---
+++ client/components/authenticationPage/appSummary.tsx
@@ -0,0 +1,7 @@
+export default function AppSummary(){
+  return (
+    <div className="flex flex-col justify-start items-start border border-red-600 w-1/2 h-screen bg-[#2B293D]">
+
+    </div>
+  )
+}
