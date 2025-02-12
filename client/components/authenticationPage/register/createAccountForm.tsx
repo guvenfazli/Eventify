81b8811685f622b5cc351e2f87c7ff3efb0b0041
Güven Fazlı <guven.fazli@gmail.com>
2025-02-12 04:25:55 +0300

    feat: Register form styles given, input and labels created.

--- client/components/authenticationPage/register/createAccountForm.tsx
+++ client/components/authenticationPage/register/createAccountForm.tsx
@@ -3,10 +3,26 @@ import AuthFormInput from "../authFormInput"
 
 export default function CreateAccountForm() {
   return (
-    <form className="flex flex-col justify-start w-full items-start">
+    <form className="flex flex-col gap-6 justify-start w-full items-start">
       <div className="flex flex-col w-full gap-2">
         <AuthFormLabel htmlFor="name">Full Name</AuthFormLabel>
-        <AuthFormInput name="name" placeHolder="Enter Your Full Name" type="text"/>
+        <AuthFormInput name="name" placeHolder="Enter Your Full Name" type="text" />
+      </div>
+
+      <div className="flex flex-col w-full gap-2">
+        <AuthFormLabel htmlFor="email">E-mail Address</AuthFormLabel>
+        <AuthFormInput name="email" placeHolder="Enter Your E-Mail" type="email" />
+      </div>
+
+      <div className="flex flex-col w-full gap-2">
+        <AuthFormLabel htmlFor="password">Password</AuthFormLabel>
+        <AuthFormInput name="password" placeHolder="Password" type="password" />
+      </div>
+
+      <div className="flex justify-center items-center w-full">
+        <button className="bg-[#2B293D] w-full py-2 text-white text-[24px] font-opensans font-bold rounded-md">
+          Create Account
+        </button>
       </div>
     </form>
   )
--- client/components/authenticationPage/register/registerSection.tsx
+++ client/components/authenticationPage/register/registerSection.tsx
@@ -1,8 +1,8 @@
 import CreateAccountForm from "./createAccountForm"
 export default function RegisterSection() {
   return (
-    <div className="flex justify-center items-start w-1/2 border border-red-600 h-screen">
-      <div className="flex flex-col items-center justify-center border border-blue-600 h-full w-7/12">
+    <div className="flex justify-center items-start w-1/2 h-screen">
+      <div className="flex flex-col items-center justify-center h-full w-7/12">
         <div className="flex w-full items-center justify-start text-[48px] font-monster text-[#2B293D] font-bold">
           <p>Create Account</p>
         </div>
