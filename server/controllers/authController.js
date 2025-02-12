981c318c75693f84e2cd3d39aad03923c60212f3
Güven Fazlı <guven.fazli@gmail.com>
2025-02-12 06:22:11 +0300

    feature: Custom error util added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,29 @@
+const { throwError } = require('../utils/throwError') // (statusCode, errorMsg)
 const bcrypt = require('bcryptjs')
 
-exports.createAccount = (req, res, next) => {
+/* Models */
+const User = require('../models/User')
+
+exports.createAccount = async (req, res, next) => {
   const { name, email, password } = req.body
   const splitted = name.split(' ')
   const lastName = splitted.length - 1
+
+  try {
+    const foundUser = await User.findOne({ email: email })
+
+    if (foundUser) {
+      throwError(410, 'User already exists!')
+    }
+
+    if (!name) {
+      throwError(410, 'Testing Custom Error!')
+    }
+
+
+  } catch (err) {
+    next(err)
+  }
 
 }
 
