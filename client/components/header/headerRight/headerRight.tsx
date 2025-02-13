84593147d9184317d04df7e65eb9c4b1f1bb53cf
Güven Fazlı <guven.fazli@gmail.com>
2025-02-13 04:21:07 +0300

    chore: shadcn installed, feat: tooltips added for icons

--- client/components/header/headerRight/headerRight.tsx
+++ client/components/header/headerRight/headerRight.tsx
@@ -1,12 +1,43 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 import Link from "next/link"
 import { IoTicketOutline, IoStarOutline } from "react-icons/io5";
 export default function HeaderRight() {
   return (
     <nav className="flex justify-around items-center text-[20px] gap-5 text-white">
       <Link href={'/createEvent'} className="font-medium">Create Event</Link>
-      <Link href={'/tickets'}><IoTicketOutline className="text-[#FFE047]" /></Link>
-      <Link href={'/interested'}><IoStarOutline className="text-[#FFE047]" /></Link>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Link href={'/tickets'}>
+              <IoTicketOutline className="text-[#FFE047]" />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Tickets</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Link href={'/interested'}>
+              <IoStarOutline className="text-[#FFE047]" />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Interested</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
       <Link href={'/signOut'} className="font-medium">Sign Out</Link>
-    </nav>
+    </nav >
   )
 }
