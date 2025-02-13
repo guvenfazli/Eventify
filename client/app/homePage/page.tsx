a6b8f98e3b6afa7e59cce53bbd7109a4df57c936
Güven Fazlı <guven.fazli@gmail.com>
2025-02-13 10:25:04 +0300

    feature: 3rd party FREE api fetching completed for country list.

--- client/app/homePage/page.tsx
+++ client/app/homePage/page.tsx
@@ -1,12 +1,36 @@
 import GreetSection from "@/components/homePage/greetSection/greetSection"
 import ExploreCategories from "@/components/homePage/exploreCategories/exploreCategories"
-export default function Page() {
-  return (
+export default async function Page() {
+
+  try {
+
+    const response = await fetch('https://restcountries.com/v3.1/region/europe')
+    if (!response.ok) {
+      const resData = await response.json()
+      const error = new Error(resData)
+      throw error
+    }
+
+    const countryList = await response.json()
+
+    return (
+      <div className="flex flex-col items-start justify-start">
+        <GreetSection countryList={countryList} />
+        <div className="flex flex-col justify-start items-start px-32 w-full">
+          <ExploreCategories />
+        </div>
+      </div>
+    )
+
+
+  } catch (err) {
     <div className="flex flex-col items-start justify-start">
       <GreetSection />
       <div className="flex flex-col justify-start items-start px-32 w-full">
-        <ExploreCategories />
+        <p>Could not fetch any countries.</p>
       </div>
     </div>
-  )
+  }
+
+
 }
--- client/components/homePage/greetSection/greetSection.tsx
+++ client/components/homePage/greetSection/greetSection.tsx
@@ -2,16 +2,16 @@
 
 import SearchSection from "./searchSection"
 
-export default function GreetSection() {
+export default function GreetSection({ countryList }: any) {
   return (
-    <div  className="flex flex-col items-center justify-center w-full relative space-y-7 min-h-[578px] bg-[url('/assets/homePage/eventPicture.jpeg')] bg-bottom bg-cover ">
+    <div className="flex flex-col items-center justify-center w-full relative space-y-7 min-h-[578px] bg-[url('/assets/homePage/eventPicture.jpeg')] bg-bottom bg-cover ">
       <div className="absolute inset-0 bg-[#2B293D] opacity-50" />
       <div className="text-[48px] text-white font-bold z-10 font-monster tracking-wider">
         <p>Don’t miss out!</p>
         <p>Explore the <span className="text-[#FFE047]">vibrant events</span> happening locally and globally.</p>
       </div>
 
-      <SearchSection />
+      <SearchSection countryList={countryList} />
 
     </div>
   )
