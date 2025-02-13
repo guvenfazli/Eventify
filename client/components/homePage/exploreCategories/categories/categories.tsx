e8dcab52801a023a152bc4f8e5a6a09587302037
Güven Fazlı <guven.fazli@gmail.com>
2025-02-13 09:33:04 +0300

    feature: categories section added, categories turned into anchor, will be fetching the filtered results

--- client/components/homePage/exploreCategories/categories/categories.tsx
+++ client/components/homePage/exploreCategories/categories/categories.tsx
@@ -4,9 +4,10 @@ export default function Categories() {
     <div className="flex w-full gap-4 justify-around items-start">
       <SingleCategory imageUrl="entertainment.jpg" category="Entertainment" />
       <SingleCategory imageUrl="meeting.jpg" category="Business & Education" />
-
-
-
+      <SingleCategory imageUrl="art.jpg" category="Art" />
+      <SingleCategory imageUrl="sports.jpg" category="Sports" />
+      <SingleCategory imageUrl="tech.jpg" category="Technology" />
+      <SingleCategory imageUrl="travel.jpg" category="Technology" />
     </div>
   )
 }
--- client/components/homePage/exploreCategories/categories/singleCategory.tsx
+++ client/components/homePage/exploreCategories/categories/singleCategory.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 interface ComponentProps {
   imageUrl: string,
   category: string
@@ -5,13 +6,13 @@ interface ComponentProps {
 
 export default function SingleCategory({ imageUrl, category }: ComponentProps) {
   return (
-    <div className="flex w-2/5 min-h-[170px] p-2 border border-red-500 flex-col justify-between items-center text-center">
+    <div className="flex w-2/5 min-h-[170px] p-2 flex-col justify-between items-center text-center gap-3">
       <div style={{ backgroundImage: `url('/assets/homePage/categories/${imageUrl}')` }} className={`border border-blue-500 min-w-[150px] min-h-[150px] bg-cover rounded-full`} />
 
 
       <div className="text-wrap">
-        <p className="font-opensans font-semibold text-[24px] text-[#2D2C3C]">{category}</p>
+        <Link href={`/results/${category}`} className="font-opensans font-semibold text-[24px] text-[#2D2C3C]">{category}</Link>
       </div>
-    </div >
+    </div>
   )
 }
