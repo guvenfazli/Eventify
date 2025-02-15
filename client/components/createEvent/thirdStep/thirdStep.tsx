47da93f50be1e2c4e402ccc3c66ea1eb4dc484c8
Güven Fazlı <guven.fazli@gmail.com>
2025-02-16 00:06:57 +0300

    feat: started to third step, created the component, folders, added title

---
+++ client/components/createEvent/thirdStep/thirdStep.tsx
@@ -0,0 +1,11 @@
+import FormSection from "../formSection"
+
+export default function ThirdStep() {
+  return (
+    <div className="flex flex-col items-start justify-start gap-2 ">
+      <FormSection formSection="What Type of Event are You Running?" />
+
+
+    </div>
+  )
+}
