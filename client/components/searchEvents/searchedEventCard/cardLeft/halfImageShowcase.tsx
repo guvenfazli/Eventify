59eca286994c237ad106684310f9e5c45eb58fde
Güven Fazlı <guven.fazli@gmail.com>
2025-02-27 04:39:51 +0300

    feat: event search card image visual bug fixed

--- client/components/searchEvents/searchedEventCard/cardLeft/halfImageShowcase.tsx
+++ client/components/searchEvents/searchedEventCard/cardLeft/halfImageShowcase.tsx
@@ -9,7 +9,7 @@ interface ComponentProps {
 export default function HalfImageShowcase({ imgURL }: ComponentProps) {
   return (
     <>
-      <Image fill src={`http://localhost:8080/${imgURL}`} alt="eventImage" className="rounded-tl-md rounded-tr-md shadow-md bg-cover bg-center" />
+      <Image fill src={`http://localhost:8080/${imgURL}`} alt="eventImage" className="rounded-tl-md rounded-tr-md shadow-md" style={{ objectFit: 'cover' }} />
     </>
   )
 }
