5eb34f95d5044b6fc5f22397a219fb394f8c7840
Güven Fazlı <guven.fazli@gmail.com>
2025-02-28 09:28:58 +0300

    fix: free and ticket price rendering bug fixed

--- client/components/createEvent/fourthStep/eventPreview/eventInformation/eventTicketInformation.tsx
+++ client/components/createEvent/fourthStep/eventPreview/eventInformation/eventTicketInformation.tsx
@@ -4,14 +4,15 @@ import { IoTicket } from "react-icons/io5";
 export default function EventTicketInformation() {
 
   const multiStepValues = useSelector((state: any) => state.rootReducer.multiFormSlice)
-
+  console.log(multiStepValues)
   return (
     <div className="flex flex-col items-start justify-start text-[#2D2C3C] font-opensans">
       <p className="text-[36px] font-bold">Ticket Information</p>
 
       <div className="flex items-center justify-start gap-3">
         <IoTicket className="text-[30px] font-semibold" />
-        <p className="font-semibold text-[24px] capitalize">Ticket Type: {multiStepValues.eventType} / {multiStepValues.ticketQuantity} x <span className="text-[20px]">{multiStepValues.ticketPrice} EUR</span></p>
+        <p className="font-semibold text-[24px] capitalize">Ticket Type: {multiStepValues.eventType} </p>
+        {multiStepValues.eventType !== "free" && <p> / {multiStepValues.ticketQuantity} x <span className="text-[20px]">{multiStepValues.ticketPrice} EUR</span ></p>}
       </div>
     </div>
   )
